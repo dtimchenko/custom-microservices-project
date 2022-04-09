855d9988cd26786a342840c1e033b2bcb026ae63
Dmytro Timchenko <zalman1992@gmail.com>
2022-04-09 19:23:08 +0300

    add JPA support. closes #5

--- customer/src/main/java/org/example/customer/Customer.java
+++ customer/src/main/java/org/example/customer/Customer.java
@@ -1,11 +1,28 @@
 package org.example.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
--- customer/src/main/java/org/example/customer/CustomerService.java
+++ customer/src/main/java/org/example/customer/CustomerService.java
@@ -3,12 +3,14 @@ package org.example.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
                 .lastName(customerRequest.lastName())
                 .email(customerRequest.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
