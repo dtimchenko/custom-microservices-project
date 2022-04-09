c8a034c8390c20ac2ee1fa46585aed59f9e3dfcc
Dmytro Timchenko <zalman1992@gmail.com>
2022-04-09 20:02:55 +0300

    add fraud service with database. closes #6

---
+++ fraud/src/main/java/com/example/fraud/FraudCheckHistory.java
@@ -0,0 +1,30 @@
+package com.example.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
