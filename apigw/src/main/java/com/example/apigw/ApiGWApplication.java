dd4b2bf1b945e502a396321fd94be9c8735df816
Dmytro Timchenko <zalman1992@gmail.com>
2022-04-15 15:50:06 +0300

    added api gateway service with auto mapping. closes #15

---
+++ apigw/src/main/java/com/example/apigw/ApiGWApplication.java
@@ -0,0 +1,15 @@
+package com.example.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGWApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class, args);
+    }
+
+}
