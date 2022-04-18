8a78055c87140edf80f39945b5e4766e6e84be42
Dmytro Timchenko <zalman1992@gmail.com>
2022-04-18 22:48:48 +0300

    added a hystrix circuit breaker. closes #28

--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.example.clients")
+@ComponentScan(basePackages = {
+        "com.example.customer",
+        "com.example.clients"
+})
 @SpringBootApplication
 public class CustomerApplication {
 
