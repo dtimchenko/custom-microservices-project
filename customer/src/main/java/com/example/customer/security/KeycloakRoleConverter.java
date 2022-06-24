cf828c9f0b7f040ecd40d819ddfe30a82b45939e
Dmytro Timchenko <zalman1992@gmail.com>
2022-06-24 21:28:04 +0300

    Added Keycloak OAuth2 Server. closes #36

---
+++ customer/src/main/java/com/example/customer/security/KeycloakRoleConverter.java
@@ -0,0 +1,27 @@
+package com.example.customer.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = (Map<String, Object>)jwt.getClaims().get("realm_access");
+
+        if(Objects.isNull(realmAccess) || realmAccess.isEmpty()){
+            return Collections.emptyList();
+        }
+
+        return ((List<String>)realmAccess.get("roles")).stream()
+                .map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+}
